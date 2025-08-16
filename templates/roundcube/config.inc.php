c584c269e6bafd2323d9998c825cb43b8194a690
Olivier van Helden (mungo) <olivier@van-helden.net>
2025-08-16 19:05:16 +0000

    add clamav/ dovecot/ mail/ postgrey/ procmailrc  roundcube/ spamassassin/ in templates

---
+++ templates/roundcube/config.inc.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ +-----------------------------------------------------------------------+
+ | Local configuration for the Roundcube Webmail installation.           |
+ |                                                                       |
+ | This is a sample configuration file only containing the minimum       |
+ | setup required for a functional installation. Copy more options       |
+ | from defaults.inc.php to this file to override the defaults.          |
+ |                                                                       |
+ | This file is part of the Roundcube Webmail client                     |
+ | Copyright (C) The Roundcube Dev Team                                  |
+ |                                                                       |
+ | Licensed under the GNU General Public License version 3 or            |
+ | any later version with exceptions for skins & plugins.                |
+ | See the README file for a full license statement.                     |
+ +-----------------------------------------------------------------------+
+*/
+
+$config = [];
+
+// Do not set db_dsnw here, use dpkg-reconfigure roundcube-core to configure database!
+include("/etc/roundcube/debian-db-roundcube.php");
+
+// IMAP host chosen to perform the log-in.
+// See defaults.inc.php for the option description.
+//$config['imap_host'] = ["ssl://mail.%s"];
+//$config['imap_host'] = ["mail.%s"];
+$config['imap_host'] = 'ssl://mail.%s';
+//$config['imap_auth_type'] = 'IMAP';
+
+// SMTP server host (for sending mails).
+// See defaults.inc.php for the option description.
+//$config['smtp_host'] =  ["ssl://mail.%s"];
+//$config['smtp_host'] =  ["ssl://mail.%s:587"];
+//$config['smtp_host'] =  ["mail.%s"];
+//'ssl:mail.magiiic.com';
+//$config['smtp_host'] =  'ssl:mail.%s';
+$config['smtp_host'] =  'ssl:mail.%s';
+$config['smtp_server'] = 'localhost';
+$config['smtp_port'] = 587;
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+
+//$config['smtp_auth_type'] = 'PLAIN';
+
+// SMTP username (if required) if you use %u as the username Roundcube
+// will use the current username for login
+//$config['smtp_user'] = '%fu';
+
+// SMTP password (if required) if you use %p as the password Roundcube
+// will use the current user's password for login
+//$config['smtp_pass'] = '%p';
+
+// provide an URL where a user can get support for this Roundcube installation
+// PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
+$config['support_url'] = '';
+
+// Name your service. This is displayed on the login screen and in the window title
+$config['product_name'] = 'Magiiic Webmail';
+
+// This key is used to encrypt the users imap password which is stored
+// in the session record. For the default cipher method it must be
+// exactly 24 characters long.
+// YOUR KEY MUST BE DIFFERENT THAN THE SAMPLE VALUE FOR SECURITY REASONS
+$config['des_key'] = '';
+
+// List of active plugins (in plugins/ directory)
+// Debian: install roundcube-plugins first to have any
+$config['plugins'] = [
+	'emoticons',
+	'thunderbird_labels',
+	'archive',
+	'zipdownload',
+];
+
+// skin name: folder from skins/
+$config['skin'] = 'elastic';
+
+// Disable spellchecking
+// Debian: spellchecking needs additional packages to be installed, or calling external APIs
+//         see defaults.inc.php for additional informations
+$config['enable_spellcheck'] = false;
+
+// the default locale setting (leave empty for auto-detection)
+// RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
+$config['language'] = 'fr_FR';
